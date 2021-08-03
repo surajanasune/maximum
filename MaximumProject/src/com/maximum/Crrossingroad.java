b6fd8258c1a6877fc277cb3415e885d72ab0594a
Suraj Anasune <Suraj Anasune@LAPTOP-O70NE2SD>
2021-08-03 21:21:17 +0530

    maximum project commit

---
+++ MaximumProject/src/com/maximum/Crrossingroad.java
@@ -0,0 +1,61 @@
+package com.maximum;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Crrossingroad {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter how many numbers you want to enter :- \n ");
+		int num = sc.nextInt();
+		int number = 0;
+		ArrayList<Integer> al = new ArrayList<>();
+		System.out.println("Enter " + num + " Numbers Only 0 or 1 : -");
+		int count = 1;
+		while (true && count <= num) {
+			int input;
+			if (sc.hasNextInt()) {
+				input = sc.nextInt();
+				if (input == 0 || input == 1) {
+					count++;
+					al.add(input);
+				} else {
+					System.out.println("Only 0 or 1 accept-> \n ");
+					input = sc.nextInt();
+					count++;
+					al.add(input);
+				}
+			} else {
+				System.out.println("That is not a number");
+				sc.next();
+			}
+		}
+
+		int countPair = getPairs(al);
+		System.out.println("The count of crossing peoples is " + countPair);
+	}
+
+	private static int getPairs(ArrayList<Integer> al) {
+		int countPair = 0;
+		for (int i = 0; i < al.size(); i++) {
+
+			int searchNum;
+			if (al.get(i) == 0) {
+				searchNum = al.get(i);
+				if (searchNum == 0) {
+					for (int j = i + 1; j < al.size(); j++) {
+						if (i == j) {
+							continue;
+						} else {
+							if (al.get(j) == 1) {
+								countPair++;
+							}
+						}
+					}
+				}
+
+			}
+		}
+		return countPair;
+	}
+}
